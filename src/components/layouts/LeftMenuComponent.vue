97c99a187efccf6a751dfcc2a003105a8e546b7e
DephPhascow <d.sinisterpsychologist@gmail.com>
2023-11-20 14:38:29 +0500

    all left menu layout

---
+++ src/components/layouts/LeftMenuComponent.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+</script>
+
+<template>
+  <main class="main">
+    <div class="main__menu">
+      <slot name="menu"></slot>
+    </div>
+    <div class="main__header">
+      <slot name="header"></slot>
+    </div>
+    <div class="main__content">
+      <slot name="content"></slot>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.main {
+  display: grid;
+  height: 100dvh;
+  grid-template-areas:
+    "menu header"
+    "menu content";
+  grid-template-columns: min-content auto;
+  grid-template-rows: min-content 1fr;
+  &__menu {
+    grid-area: menu;
+  }
+  &__header {
+    grid-area: header;
+  }
+  &__content {
+    grid-area: content;
+    background-color: white;
+    border-radius: 8px;
+    height: 100%;
+  }
+  @media(max-width: 1000px) {
+    grid-template-areas:
+      "header"
+      "content"
+      "menu";
+    grid-template-columns: auto;
+    grid-template-rows: 10% 80% 10%;
+    &__content {
+      overflow: auto;
+    }
+    &__menu {
+      box-shadow: -1px -1px 10px rgba(0,0,0,0.75);
+    }
+  }
+}
+</style>
+
+<script lang="ts">
+export default defineComponent({
+  name: "LeftMenuComponent",
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,55 @@
 <template>  
-  <MainLayout>
+  <LeftMenuComponent>
+    <template #menu>Menu</template>
+    <template #header>Header</template>
     <template #content>
       {{ $t("example") }}
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
+      <InputTextComponent v-model="text" label="тест"  />
       <InputTextComponent v-model="text" label="тест"  />
       <ButtonComponent text="Click me" class="button__primary" :disabled="disable" @click="editableRow = !editableRow" />
       <TableEditableComponent :headers="headers" v-model="data" style="width: 400px !important;" :selectable="true" :tools="true" :editableRow="editableRow" />
     </template>
-  </MainLayout>
+  </LeftMenuComponent>
 </template>
 <script lang="ts">
 import InputTextComponent from "@/components/elements/InputTextComponent.vue";
 import ButtonComponent from "@/components/elements/ButtonComponent.vue";
-import MainLayout from "@/components/layouts/MainComponent.vue";
+import LeftMenuComponent from "@/components/layouts/LeftMenuComponent.vue";
 import { defineComponent } from "vue";
 import TableEditableComponent from "@/components/elements/TableEditableComponent.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    MainLayout,
+    LeftMenuComponent,
     InputTextComponent,
     ButtonComponent,
     TableEditableComponent,
